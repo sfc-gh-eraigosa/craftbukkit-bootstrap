6fc72664f68246f40e4ad2b5b7e1abfd66d0eb7f
wenlock <edward.raigosa@gmail.com>
2013-09-08 20:28:49 -0700

    Update java.pp

--- puppet/java.pp
+++ puppet/java.pp
@@ -1,20 +1,9 @@
 class stableJDK {
     class{ 'java':
-        version => '1.7.0_21',
+        version => '1.7.0_25',
         tarfile =>  $::architecture ? {
-            'amd64' => 'jdk-7u21-linux-x64.tar.gz',
-            default => 'jdk-7u21-linux-i586.tar.gz',
-        },
-        force   => false
-    }
-}
-
-class stableJRE {
-    class{ 'java':
-        version => '1.7.0_21',
-        tarfile =>  $::architecture ? {
-            'amd64' => 'jre-7u21-linux-x64.tar.gz',
-            default => 'jre-7u21-linux-i586.tar.gz',
+            'amd64' => 'jdk-7u25-linux-x64.tar.gz',
+            default => 'jdk-7u25-linux-i586.tar.gz',
         },
         force   => false
     }

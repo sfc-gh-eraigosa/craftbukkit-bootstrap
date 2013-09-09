d9f745afc5a6f7493fe25d5952296068d964fcfe
wenlock <edward.raigosa@gmail.com>
2013-09-08 21:20:07 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -31,8 +31,8 @@ if [[ ! -d ./puppet-java ]] ; then
 git clone https://github.com/objectcomputing/puppet-java
 fi
 
-if [[ ! -f ./puppet-java/files/jdk-${JAVA_VERSION}-linux-x64.tar.gz ]] ; then
-wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-${JAVA_BUILD}/jdk-${JAVA_VERSION}-linux-x64.tar.gz" -O ./puppet-java/files/jdk-${JAVA_VERSION}-linux-x64.tar.gz
+if [[ ! -f ./puppet-java/jdk-${JAVA_VERSION}-linux-x64.tar.gz ]] ; then
+wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-${JAVA_BUILD}/jdk-${JAVA_VERSION}-linux-x64.tar.gz" -O ./puppet-java/jdk-${JAVA_VERSION}-linux-x64.tar.gz
 fi
 
 sudo puppet apply --modulepath=$PUPPET_MODULES ./java.pp

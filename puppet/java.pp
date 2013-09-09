01655331a904314fe513ff8eb0780aacd95d4c3f
wenlock <edward.raigosa@gmail.com>
2013-09-08 20:18:12 -0700

    Update java.pp

--- puppet/java.pp
+++ puppet/java.pp
@@ -1,3 +1,21 @@
- package { 'libbcpg-java':
-      ensure => present,
+class stableJDK {
+    class{ 'java':
+        version => '1.7.0_21',
+        tarfile =>  $::architecture ? {
+            'amd64' => 'jdk-7u21-linux-x64.tar.gz',
+            default => 'jdk-7u21-linux-i586.tar.gz',
+        },
+        force   => false
     }
+}
+
+class stableJRE {
+    class{ 'java':
+        version => '1.7.0_21',
+        tarfile =>  $::architecture ? {
+            'amd64' => 'jre-7u21-linux-x64.tar.gz',
+            default => 'jre-7u21-linux-i586.tar.gz',
+        },
+        force   => false
+    }
+}

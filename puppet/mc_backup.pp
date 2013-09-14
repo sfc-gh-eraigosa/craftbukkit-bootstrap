a70cd980ae23ab0615422e2154707346791dcb7a
Connie Raigosa <craigosa@gmail.com>
2013-09-14 22:26:36 +0000

    path error to script for cron

--- puppet/mc_backup.pp
+++ puppet/mc_backup.pp
@@ -1,6 +1,7 @@
+$mc_install_root='/home/bitnami/craftbukkit'
 notice("installing hourly crontab for : /home/${::id}/craftbukkit/start.sh backup")
 cron { 'mc_backup':
-    command => "/home/${::id}/craftbukkit/start.sh backup",
+    command => "/bin/bash ${mc_install_root}/start.sh backup",
     hour    => '*/1',
     user    => "${::id}",
     ensure  => present,

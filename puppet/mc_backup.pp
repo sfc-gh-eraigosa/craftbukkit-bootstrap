f12c1926adbce6093470802dae8568c88262e33e
Connie Raigosa <craigosa@gmail.com>
2013-09-15 15:09:47 +0000

    hourly backups were failing. corrected crontab

--- puppet/mc_backup.pp
+++ puppet/mc_backup.pp
@@ -2,7 +2,7 @@ $mc_install_root='/home/bitnami/craftbukkit'
 notice("installing hourly crontab for : /home/${::id}/craftbukkit/start.sh backup")
 cron { 'mc_backup':
     command => "/bin/bash ${mc_install_root}/start.sh backup",
-    hour    => '*/60',
+    minute  => '*/60',
     user    => "${::id}",
     ensure  => present,
   }

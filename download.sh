1a433f09c2645facb866084b48e9ee4a7f5e3b78
J Raigosa <Jonathan R@Jonathan.(none)>
2014-06-22 22:56:34 -0700

    update download and start.sh to work with windows

--- download.sh
+++ download.sh
@@ -1,5 +1,33 @@
-CRAFT_BUKKIT_DATE=02253
-CRAFT_BUKKIT_VER=1.6.2-R0.1
+CRAFT_BUKKIT_DATE=02633
+CRAFT_BUKKIT_VER=1.7.9-R0.3
+CRAFT_BUKKIT_REL=dev
+FACTER_BIN=$(which facter)
+if [ $? != 0 ] ; then
+  echo "ERROR: please install facter first with gem install puppet"
+  exit 1
+fi
+PUPPET_BIN=$(which puppet)
+if [ $? != 0 ] ; then
+  echo "ERROR: please install puppet first with gem install puppet"
+  exit 1
+fi
+
+if [ "$1" != "" ] ; then
+  CRAFT_BUKKIT_DATE=$1
+fi
+if [ "$2" != "" ] ; then
+  CRAFT_BUKKIT_VER=$2
+fi
+function dosudo
+{
+  uname -s|grep -i win > /dev/null
+  if [ $? != 0 ] ; then
+     sudo "$@"
+  else 
+     eval "$@"
+  fi
+}
+
 JAVA_VERSION=7u25
 JAVA_BUILD=b15
 pushd "$(dirname "$0")"
@@ -7,9 +35,11 @@ CURRENT_DIR=$(pwd)
 [[ ! -d ./server ]] && mkdir server
 [[ ! -d ./backup ]] && mkdir backup
 if [[ ! -f ./server/craftbukkit_${CRAFT_BUKKIT_VER}.jar ]] ; then
-wget "http://dl.bukkit.org/downloads/craftbukkit/get/${CRAFT_BUKKIT_DATE}_${CRAFT_BUKKIT_VER}/craftbukkit-beta.jar" -O "./server/craftbukkit_${CRAFT_BUKKIT_VER}.jar"
+wget "http://dl.bukkit.org/downloads/craftbukkit/get/${CRAFT_BUKKIT_DATE}_${CRAFT_BUKKIT_VER}/craftbukkit-${CRAFT_BUKKIT_REL}.jar" -O "./server/craftbukkit_${CRAFT_BUKKIT_VER}.jar"
 
-ln -s $CURRENT_DIR/server/craftbukkit_${CRAFT_BUKKIT_VER}.jar $CURRENT_DIR/server/craftbukkit.jar
+fi
+if [ ! -f "$CURRENT_DIR/server/craftbukkit.jar" ] ; then
+  ln -s "$CURRENT_DIR/server/craftbukkit_${CRAFT_BUKKIT_VER}.jar" "$CURRENT_DIR/server/craftbukkit.jar"
 fi
 
 if [[ ! -d ./config ]] ; then
@@ -20,7 +50,7 @@ git pull
 popd
 fi
 # best puppet bootstrap script ever!
-sudo ./config/install_puppet.sh
+dosudo ./config/install_puppet.sh
 
 # jdk deploy for java
 [[ ! -d ./puppet ]] && mkdir puppet
@@ -35,9 +65,19 @@ pushd ./puppet
 #wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-${JAVA_BUILD}/jdk-${JAVA_VERSION}-linux-x64.tar.gz" -O ./puppet-java/jdk-${JAVA_VERSION}-linux-x64.tar.gz
 #fi
 
-sudo puppet apply --modulepath=$PUPPET_MODULES ./java.pp 2>&1 | tee -a java_install.log
-sudo rm -f /etc/alternatives/java
-sudo ln -s /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java /etc/alternatives/java
+
+uname -s |grep -i win > /dev/null
+if [ $? != 0 ] ; then
+  dosudo puppet apply --modulepath=$PUPPET_MODULES ./java.pp 2>&1 | tee -a java_install.log
+  dosudo rm -f /etc/alternatives/java
+  dosudo ln -s /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java /etc/alternatives/java
+else
+  JAVA_BIN=$(which java)
+  if [ $? != 0 ] ; then
+      echo "ERROR : missing java, please install"
+      exit 1
+  fi
+fi
 
 popd
 
